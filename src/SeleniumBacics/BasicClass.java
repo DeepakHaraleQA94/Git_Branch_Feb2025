2f7211ddf5e898c5d1a5abc9cd1cd926a4dabc7c
DeepakHaraleQA94 <haraledeepak6@gmail.com>
2025-02-24 19:47:19 +0530

    code

--- src/SeleniumBacics/BasicClass.java
+++ src/SeleniumBacics/BasicClass.java
@@ -7,7 +7,7 @@ public class BasicClass {
 
 	public static void main(String[] args) {
 
-		System.out.println("local machine2");
+//		System.out.println("local machine2");
 
 		System.out.println("local machine3");
 		WebDriver driver = new ChromeDriver();
