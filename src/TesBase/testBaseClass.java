f8334b22a947d345e122b82b2e1cb580df3f3e76
DeepakHaraleQA94 <haraledeepak6@gmail.com>
2025-03-05 20:30:31 +0530

    pom class, test class, proprties file , tesBase class created

---
+++ src/TesBase/testBaseClass.java
@@ -0,0 +1,31 @@
+package TesBase;
+
+import static org.testng.Assert.ARRAY_MISMATCH_TEMPLATE;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class testBaseClass {
+	public static Properties prop;
+public static WebDriver driver;
+public testBaseClass() throws IOException{
+	 prop = new Properties();
+	FileInputStream file = new FileInputStream("C:\\Users\\Sarvadnya\\eclipse-workspace\\Selenium_Jala_Feb_2025\\src\\propertyFile\\config.properties");
+    prop.load(file);
+
+}
+
+	public void intilization() {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://magnus.jalatechnologies.com/");
+		
+	}
+
+
+}
+
---
+++ src/TestClass/LoginTestPage.java
@@ -0,0 +1,58 @@
+package TestClass;
+
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import POM.LoginPage;
+import TesBase.testBaseClass;
+
+public class LoginTestPage extends testBaseClass {
+	
+	public LoginTestPage() throws IOException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	LoginPage login;
+	@BeforeMethod
+	public void setUp() throws IOException {
+		intilization();
+		 login = new LoginPage();
+	}
+
+	@Test(priority = 1)
+	public void verifyLoginPage() throws InterruptedException {
+		login.validateLoginpage(prop.getProperty("Username"),prop.getProperty("Password"));
+		Thread.sleep(3000);
+		WebElement text = driver.findElement(By.xpath("//h1[text()='Welcome to JALA Academy']"));
+		String homeText = text.getText();
+		Assert.assertEquals(homeText, "Welcome to JALA Academy", "text not matched");
+		Reporter.log("text matched", true);
+		
+	}
+	
+	@Test(priority = 2)
+	public void validateInvalidCredential() throws InterruptedException {
+		login.validateLoginpage("Admin", "admin123");
+		Thread.sleep(3000);
+		WebElement toster = driver.findElement(By.xpath("//div[@class='toast-title']"));
+	 	String errorMessage = toster.getText();
+	 	Assert.assertEquals(errorMessage, "Invalid Email address or Password.", "valid credential");
+	 	Reporter.log("Invalid credential",true);
+	}
+	
+	@AfterMethod
+	public void tearDown() {
+		driver.close();
+		
+	}
+	
+	
+}
